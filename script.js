4da47f6fc6960914b6193c0e2a22cb43766fdb53
ReginaldGreen02 <115752371+ReginaldGreen02@users.noreply.github.com>
2023-09-24 21:56:34 -0500

    Adding C# and JavaScript files

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("button1").addEventListener("click", function() {
+    alert("Button 1 was clicked!");
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    alert("Button 2 was clicked!");
+});
